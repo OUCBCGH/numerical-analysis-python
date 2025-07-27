0ea56e0e3a23a2ce2ccf79dc27c55515c5b02bfc
OUCBCGH <danielkretz31@gmail.com>
2025-07-27 12:50:34 +0800

    20250727-整理文件第一次提交

---
+++ code/matrix_eigenvalue_calculation_10/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+import sys
+sys.path.append(r'/home/ld/numerical-analysis-python/NumericalCalculationMethod')
+"""
+@author:Lenovo
+@file:__init__.py.py
+@time:2021/08/25
+"""
---
+++ code/numerical_optimization_13/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+import sys
+sys.path.append(r'/home/ld/numerical-analysis-python/NumericalCalculationMethod')
+"""
+@file_name: __init__.py.py
+@time: 2022-09-09
+@IDE: PyCharm  Python: 3.9.7
+@copyright: http://maths.xynu.edu.cn
+"""
