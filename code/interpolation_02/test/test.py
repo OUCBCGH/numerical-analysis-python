e6c8421b6ee231bdaeac56659afc81152ec47a9e
OUCBCGH <danielkretz31@gmail.com>
2025-08-05 20:27:45 +0800

    20250805-第二次提交

--- code/interpolation_02/test/test.py
+++
@@ -1,8 +0,0 @@
-# import  matplotlib.pyplot as plt
-# plt.rcParams['font.sans-serif'] = ['SimHei']  # 中文字体设置
-# plt.title('中华人民共和国')
-# plt.plot([1,2,3,4],['一','二','三','四'],color="b",linestyle = ":")
-# plt.savefig("output.png", dpi=300, bbox_inches='tight', transparent=True)
-
-from matplotlib.font_manager import rebuild
-_rebuild() #reload一下
